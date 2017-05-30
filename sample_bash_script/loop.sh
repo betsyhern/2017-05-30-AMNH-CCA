30e8ec1e51d2b290138e90a1672832f048bbe3d1
BabyOstrich <Camille Avestruz>
2017-05-30 13:17:00 -0500

    Added my first bash loop

---
+++ sample_bash_script/loop.sh
@@ -0,0 +1,23 @@
+# Comments are preceded by the hash symbol.  You can use comments as documentation, or just to temporarily remove lines from being executed.
+
+# This is a sample loop in bash.
+
+# Exercise: Modify this to print 1 through 10 using the curly brace notation for iterating over a range.  And, make an additional nest to print a through g.
+
+for i in {1..10};
+do 
+echo $i;
+for j in {a..g};
+do echo $j;
+done
+done
+
+for i in {1..10};
+do 
+echo $i;
+done
+
+for i in {a..g};
+do 
+echo $i
+done
